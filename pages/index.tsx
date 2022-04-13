d4efe89f62958e182a4a6d09e50a79d981b0d49d
Anton Palermo <ANPBusinessmail@gmail.com>
2022-04-13 17:48:51 +0800

    initialize app

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Welcome from 'components/Welcome'
+
+const Home = () => {
+  return (
+    <>
+      <h1>Hi user</h1>
+      <Welcome />
+    </>
+  )
+}
+
+export default Home
