e518fb95fb558fcdb0095665a258d048e346a7eb
Anton Palermo <ANPBusinessmail@gmail.com>
2022-04-14 00:06:35 +0800

    init prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+import { prisma } from '@db/prisma'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   return await NextAuth(req, res, {
+    adapter: PrismaAdapter(prisma),
     providers: [
       GoogleProvider({
         clientId: `${process.env.GOOGLE_CLIENT_ID}`,
